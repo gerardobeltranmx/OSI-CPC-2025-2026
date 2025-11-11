579114f8e7702ffb8edd46430b3b40e77328ab61
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-11-10 18:50:17 -0700

    --

--- Elementos básicos del lenguaje C++/F. Tres Numeros Al Reves/prog.cpp
+++ Elementos básicos del lenguaje C++/F. Tres Numeros Al Reves/prog.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-
+    int a, b, c;
+    
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    
+    cout << c << endl;
+    cout << b << endl;
+    cout << a << endl;
+    
+    
     return 0;
 }
