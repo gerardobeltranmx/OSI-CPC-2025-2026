4c25cf27e9391f82a8022430994a46c509d27730
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-11-10 19:58:46 -0700

    --

---
+++ Elementos básicos del lenguaje C++/I. suma simple/prog.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    
+    cin >> n;
+    //cout << (n + 1) << endl;
+    
+    //n = n + 1;
+    //cout << n << endl;
+    
+    // n++;
+    // cout << n << endl;
+    
+    n+=1; // n = n + 1
+    cout << n << endl;
+    
+    return 0;
+}
