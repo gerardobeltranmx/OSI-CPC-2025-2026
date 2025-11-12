f76151ad0087494843338dc654d222471d09a14c
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-11-11 19:10:10 -0700

    --

---
+++ Elementos básicos del lenguaje C++/L. cambia a mayuscula/prog.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char letra ;
+    
+    cin >> letra;
+
+    letra=letra-32;
+    
+    cout << letra << endl;
+
+    
+    
+    return 0;
+}
