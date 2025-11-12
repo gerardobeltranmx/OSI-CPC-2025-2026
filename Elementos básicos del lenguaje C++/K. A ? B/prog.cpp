1144209fde429963642d439e1ea8c0f40cd45ee4
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-11-11 18:54:53 -0700

    --

--- Elementos básicos del lenguaje C++/K. A ? B/prog.cpp
+++ Elementos básicos del lenguaje C++/K. A ? B/prog.cpp
@@ -2,26 +2,23 @@
 using namespace std;
 int main()
 {
-    int A, B, C;
+    int A, B;
 
     cin >> A >> B;
-    C = A + B;
-    cout << C << endl;
+   // C = A + B;
+    cout << (A + B) << endl;
 
-    C = A - B;
-    cout << C << endl;
+    // C = A - B;
+    cout << (A - B) << endl;
 
-    C = A * B;
-    cout << C << endl;
+    // C = A * B;
+    cout << (A * B) << endl;
 
-    C = A / B;
-    cout << C << endl;
+    // C = A / B;
+    cout << (A / B) << endl;
 
-    C = A % B;
-    cout << C << endl;
-
-
-
+    // C = A % B;
+    cout << (A % B)  << endl;
 
     return 0;
 }
