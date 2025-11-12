187f3ee6183a611ae5a9cdd43dbda3ab4dc34c76
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-11-11 19:56:16 -0700

    --

---
+++ Elementos básicos del lenguaje C++/O. Área de un Círculo - Ejemplo 1/prog.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double radio, area;
+    
+    const double PI = 3.1416;
+    
+    cin >> radio;
+    
+    area = PI * radio * radio;
+    
+    cout << fixed<< setprecision(2)<< area << endl;   
+    
+    return 0;
+}
