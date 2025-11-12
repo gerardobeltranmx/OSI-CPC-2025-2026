a58659a83c7bdef6d9a35cb4c9551a24379fceca
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-11-11 19:23:49 -0700

    --

---
+++ Elementos básicos del lenguaje C++/M. Dólares/prog.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    unsigned short N, K, P;
+
+    cin >> N;
+    cin >> K;
+
+    P = N * K;
+
+    cout << P << endl;
+
+    return 0;
+}
