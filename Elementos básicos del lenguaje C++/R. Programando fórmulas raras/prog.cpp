0ea9295c8530cb672187bf50306f348ff8d33557
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-11-24 19:23:08 -0700

    --

---
+++ Elementos básicos del lenguaje C++/R. Programando fórmulas raras/prog.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double x, y ,z, resultado;
+   // const double PI = 3.1416;
+   // datos de entrada 
+   cin >> x >> y >> z;
+
+    // formula rara 
+    resultado =  ( x + x * (y + z*z) ) / ((x + M_PI)* (y + M_PI));
+
+    // salida de resultados
+    cout << fixed << setprecision(6) << resultado << endl;
+
+    return 0;
+}
+
