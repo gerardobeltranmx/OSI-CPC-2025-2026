44adca0890b4edb2b33057b8f7e6bcff04084b08
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-11-25 19:26:13 -0700

    --

---
+++ 02 Estructuras de control condicional /D. El mayor de dos números/prog.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x1, x2;
+    cin >> x1;
+    cin >> x2;
+    if (x1 > x2){
+        cout << x1 << endl;
+    }
+    else{
+        cout << x2 << endl;
+    }
+
+    return 0;
+}
