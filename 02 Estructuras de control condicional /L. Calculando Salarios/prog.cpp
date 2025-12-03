7f1fcc25a87a659efa4ea0e312ca7c920c81d588
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-12-02 19:08:56 -0700

    --

---
+++ 02 Estructuras de control condicional /L. Calculando Salarios/prog.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int s, h, t;
+    cin >> s >> h;
+
+    if (h<=40){
+        t = s * h;
+    }
+    else if (h<=48){
+        t = s * 40 + 2 * s * (h - 40);
+    }
+    else {
+        t = s * 40 + 2 * s * 8 + 3 * s * (h - 48);
+    }
+
+    cout << t << endl;
+
+
+    return 0;
+}
