a683d62fda068e3b92d26cf60bf9c616175240bd
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-12-08 19:47:21 -0700

    --

---
+++ 03 Estructuras de control repetitivas/E. A pares y nones/prog.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N, i, C, sumaA=0, sumaN=0, cuantaA=0;
+    int promedioA, promedioN;
+    cin >> N;
+
+    for (i=1; i<=N; i++){
+
+        cin >> C;
+
+         if (C % 2 == 0){
+            sumaA=sumaA+C;
+            cuentaA++;
+         }
+         else {
+            sumaN=sumaN+C;
+            cuentaN++;
+         }
+    }     
+    
+    promedioA = sumaA / cuentaA;
+    promedioN = sumaN / cuentaN;
+
+    if (promedioA > promedioN){
+        cout << "APARICIO" << endl;
+    }
+    else if (promedioN > promedioA){
+        cout << "NONILA" << endl;
+    }
+    else{
+        cout << "EMPATE!" << endl;
+    }
+
+  
+    return 0;
+}
