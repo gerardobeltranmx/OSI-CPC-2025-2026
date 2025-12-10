5c9a2a8b7680111028b182abe517669ad8fab8ce
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-12-09 18:19:36 -0700

    --

---
+++ 03 Estructuras de control repetitivas/F. Ciclo mientras no cero/prog-v2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int dato, suma=0;
+
+    cin >> dato;  
+    suma = dato;  
+    while(dato!=0){
+
+        cin >> dato;
+
+        suma = suma + dato;
+    }
+    
+    cout << suma << endl;    
+    
+    return 0;
+}
