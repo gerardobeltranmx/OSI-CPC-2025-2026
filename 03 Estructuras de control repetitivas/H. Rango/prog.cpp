e02d0b0e7b1c5e8d3e1dd9982410ac4baec2e94a
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-12-09 19:03:28 -0700

    --

---
+++ 03 Estructuras de control repetitivas/H. Rango/prog.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+
+    cin >> n;
+    cin >> m;
+
+    for (int i=n; i<=m; i++){
+        cout << i << endl;
+    }
+
+    return 0;
+}
