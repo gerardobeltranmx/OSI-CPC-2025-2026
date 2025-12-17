d87712c774293692164952f8bfbb6e05a21be0dd
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2025-12-16 18:21:23 -0700

    --

---
+++ 03 Estructuras de control repetitivas/O. Imprime pares en rango/prog.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cin >> N >> M;
+
+    for (int i=N; i<=M; i++){
+
+        if (i % 2 == 0) // es par?
+            cout << i << endl;
+
+    }
+
+
+    return 0;
+}
