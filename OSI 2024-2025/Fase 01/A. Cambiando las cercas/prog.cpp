982b4ac0094d99b2c7b8d35971465e4cb1a86470
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2026-01-06 18:51:51 -0700

    --

---
+++ OSI 2024-2025/Fase 01/A. Cambiando las cercas/prog.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int p, a, b, m;
+    /*
+      - a es la cerca corta y b es la cerca larga
+      p = 5*a + 4*b
+      2*b=3*a
+      b = 3*a/2
+      p = 5*a + 4*3*a/2
+      p = 5*a + 6*a
+      p = 11*a
+      a  = p / 11
+      2*b = 3*p/11
+      b = 3*p/11/2
+    */
+      cin >> p; // entrada del perimetro  
+      a  = p / 11;
+      b = 3*p/11/2;
+      m = 9*a + 6*b;
+
+      cout << m << endl;  // metros 
+
+    
+    return 0;
+}
