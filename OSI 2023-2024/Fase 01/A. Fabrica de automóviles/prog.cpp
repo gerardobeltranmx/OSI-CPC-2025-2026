7e3abb3c490c718dcf7314a6ff3d5cbe1e97e551
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2026-01-13 18:59:51 -0700

    --

--- OSI 2023-2024/Fase 01/A. Fabrica de automóviles/prog.cpp
+++ OSI 2023-2024/Fase 01/A. Fabrica de automóviles/prog.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 int main()
 {
+    int u, t, total, min_dia = 60*24;
+    int d, h, m;
+    cin >> u >> t;
 
-    
+    total = u * t; // total de minutos de fabricación
+
+    d = total / min_dia; // dias de fabricación
+
+    total = total % min_dia; // minutos restantes
+
+    h = total / 60; // horas de fabricación
+
+    m = total % 60; // min de fabricación
+
+    cout << d << " " << h << " " << m << endl;
+
     return 0;
 }
