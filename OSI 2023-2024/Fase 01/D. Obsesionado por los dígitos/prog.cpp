b598aef038db5b0ce306cc10c36bee6b721929af
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2026-01-19 18:57:25 -0700

    --

---
+++ OSI 2023-2024/Fase 01/D. Obsesionado por los dígitos/prog.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, d, digito, contador=0;
+    cin >> n >> d;
+
+    while ( n > 0){
+        digito = n % 10; // extrae el digito de la derecha
+
+        if ( digito == d ) contador++;
+
+        n = n / 10; // elimina el digito de la derecha
+    }
+
+    switch(contador){
+        case 0: 
+            cout << "No se encontró el dígito " << d << endl;
+            break;
+        case 1:
+            cout << "Se encontró una vez el dígito " << d << endl;
+            break;      
+        default :
+            cout << "Se encontró "  << contador << " veces el dígito " << d << endl;
+            break;
+    }
+
+
+
+
+
+    
+    return 0;
+}
