3d7a2b42e044ba5d7a9a3d6bd357224afd4cbe2d
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2026-01-20 19:19:14 -0700

    --

--- OSI 2022-2023/Fase 01/C. Edificación Virtual/prog.cpp
+++ OSI 2022-2023/Fase 01/C. Edificación Virtual/prog.cpp
@@ -4,13 +4,14 @@ int main()
 {
     int N, pisos=0;
 
-    cin >> N;
+    //cin >> N;
+    scanf("%d", &N);
     while (N > 0){
       N = N - (pisos + 1);
       pisos++;
     }    
 
-    cout << pisos << endl;
-
+    //cout << pisos << endl;
+    printf("%d\n",pisos );
     return 0;
 }
