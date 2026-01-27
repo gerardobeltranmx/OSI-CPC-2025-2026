eda379c7a1e4c53c7d3734c9c06e200a6dc4ed23
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2026-01-26 19:27:38 -0700

    --

--- 04 Arreglos/C. Promedio reloaded/prog.cpp
+++ 04 Arreglos/C. Promedio reloaded/prog.cpp
@@ -23,3 +23,16 @@ int main()
 
     return 0;
 }
+/*
+cin >> N >> A >> B;
+
+sumaA = A*(A+1) / 2;
+
+sumaB = B*(B+1) / 2;
+
+while (N<1000){
+    if (N%2 == 0) N=N+sumaA;
+    else
+        N=N+SumaB;
+}
+*/
