5e14e744f19d091b5dd036fee41f37558e1fc1e3
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2026-02-09 18:28:22 -0700

    --

---
+++ 04 Arreglos/H. pb Frecuencias/prog.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int P, n, v;
+
+    cin >> P >> n;
+
+    int profes[P];
+    // aseguramos que tengan ceros votos los profesores al inicio
+    for (int i=0; i<P; i++){
+        profes[i] = 0;
+    }
+
+    for (int i=0; i<n; i++){ //procesar las encuestas
+        cin >> v;
+        profes[v-1]++;
+    }
+
+    for (int i=0; i<P; i++ ){
+        cout << i+1 <<" " << profes[i] << endl;
+    }
+
+    
+    return 0;
+}
