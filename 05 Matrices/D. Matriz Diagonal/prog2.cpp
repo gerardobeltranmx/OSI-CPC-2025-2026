95793abb210169b1b8df81de3e977c7733aa1655
Gerardo Beltrán <gerardobeltranmx@gmail.com>
2026-03-02 19:12:13 -0700

    --

---
+++ 05 Matrices/D. Matriz Diagonal/prog2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N, valor;
+    bool estado=true;
+    int m[100][100];
+    cin >> N;
+   
+
+    // entrada
+    for (int i=0; i<N; i++){
+        for(int j=0;j<N; j++){
+            cin >> m [i][j];
+        }
+    }
+     valor = m[0][0]; 
+    // analizamos la diagonal 
+    for (int i=1; i<N ; i++){
+
+        if (valor!=m[i][i]){
+            estado=false;
+            break;
+        }    
+    }
+    // salida
+    if (estado==true)
+        cout << "SI" << endl;
+    else    
+        cout << "NO" << endl;    
+
+    
+    return 0;
+}
